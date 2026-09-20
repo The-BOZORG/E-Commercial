feadfe0bd25eb44cf589cbf886641a4998ab454e
soroush <soroushfalaki24@hotmail.com>
2026-09-20 16:49:36 +0330

    setup health check for database-redis

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 
-@Controller('health')
+import { RedisService } from 'src/redis/redis.service';
+
+@Controller('health') //GET http://localhost:3000/api/v1/health
 export class HealthController {
-  constructor() {}
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly database: TypeOrmHealthIndicator,
+    private readonly redis: RedisService,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.database.pingCheck('database'),
+
+      async () => {
+        const result = await this.redis.ping();
+
+        if (result !== 'PONG') throw new Error('redis is not healthy');
+
+        return {
+          redis: {
+            status: 'up',
+          },
+        };
+      },
+    ]);
+  }
 }
