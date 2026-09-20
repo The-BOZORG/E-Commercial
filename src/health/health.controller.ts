d8454bcace4dd3d5df864bdd029ef6c7539b5f6f
soroush <soroushfalaki24@hotmail.com>
2026-09-20 12:50:25 +0330

    fix health

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,7 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { HealthService } from './health.service';
 
 @Controller('health')
 export class HealthController {
-  constructor(private readonly healthService: HealthService) {}
+  constructor() {}
 }
